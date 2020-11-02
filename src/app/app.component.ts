433792d7de14ff5ba5883e74fbb8241fc33d849d
Fabian Eichhorner <fabian.eichhorner@gmail.com>
2020-11-02 16:44:28 +0100

    add chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  VERSION,
+  ViewChild
+} from "@angular/core";
+import {
+  Chart,
+  IChartConfiguration,
+  LineController,
+  LinearScale,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  PointElement,
+  LineElement
+} from "chart.js";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +24,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testChart';
+
+  public chart: Chart<"line">;
+
+  @ViewChild("myChart")
+  public ctx: ElementRef;
+
+  constructor() {
+    Chart.register(
+      LineController,
+      LinearScale,
+      CategoryScale,
+      Tooltip,
+      Legend,
+      PointElement,
+      LineElement
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.chart = new Chart(
+      this.ctx.nativeElement as HTMLCanvasElement,
+      {
+        type: "line",
+        data: {
+          labels: ["x", "y"],
+          datasets: [
+            {
+              label: "dataset1",
+              data: [{ x: 0, y: 1 }, { x: 1, y: 1 }]
+            }
+          ]
+        }
+      } as IChartConfiguration<"line">
+    );
+  }
 }
